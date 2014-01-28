398784ca241749dfaef30c23d25f895beaa281cf
unknown <robinsom16@EN205-15825.campus.up.edu>
2014-01-28 15:11:36 -0800

    Abstracted more of the RandomAgent class to the Agent class so that both the Random Agent and the Smart Agent are able to use methods that pertain to both.

--- RandomAgent.java
+++ RandomAgent.java
@@ -18,60 +18,6 @@ public class RandomAgent extends Agent{
 		super(environment);
 	}
 	
-	/**
-	 * this method finds the random path to the goal
-	 */
-	public void findRandomPath(){
-		
-		int length = this.alphabet.length;  // # char in alpha
-		char letter;
-		int count = 2;  //starting at 2 because this is the next state in the memory for the episodes 
-		
-		do{
-			letter = randomChar(length);
-			sensor = this.env.tick(letter);	//updates sensor
-			//encode sensors to make into an episode to store in memory
-			int encodedSensorValue = encodeSensors(sensor);
-			episodicMemory.add(new Episode(letter, encodedSensorValue, count));
-			
-			
-		}while(!sensor[IS_GOAL]);
-	}
-	
-	/**
-	 * 
-	 */
-	private int encodeSensors(boolean[] sensors){
-		
-		int encodedResult;
-		if(sensors[IS_GOAL]) encodedResult = MYSTERY_AND_GOAL_ON;
-		else if(sensors[IS_NEW_STATE]) encodedResult = MYSTERY_SENSOR_ON;
-		else encodedResult = NO_SENSORS_ON;
-		
-		return encodedResult;
-	}
-	
-	
-	/**
-	 * returns a random char that is in the alphabet that the agent is using
-	 */
-	private char randomChar(int length){
-		
-		int randomLetter = (int)(Math.random()*length);
-		char letter = this.alphabet[randomLetter];
-		return letter; 
-	}
-	
-	/**
-	 * printPath
-	 */
-	public void printPath(){
-		for(int i = 0; i<this.episodicMemory.size();i++){
-			System.out.print(this.episodicMemory.get(i).command);
-		}
-		System.out.println();
-		System.out.println(this.episodicMemory.size()-1);
-	}
 	
 	/**
 	 * main
