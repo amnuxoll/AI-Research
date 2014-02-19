757ea449df5e16567ccabff6831fbcd680932d6e
Max Robinson <maxrgit@gmail.com>
2014-02-18 16:33:20 -0800

    Update to Last Commit
    Added an untracked file.

---
+++ StateID.java
@@ -0,0 +1,14 @@
+package randomPackage;
+
+public class StateID {
+	
+	private int stateID;
+	
+	public StateID(int inputStateID){
+		stateID = inputStateID;
+	}
+	
+	public int get(){ return stateID;}
+	public void set(int inputStateID){stateID = inputStateID;} 
+
+}
